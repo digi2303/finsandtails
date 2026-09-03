a326e2bae78fa28b272e3118a74aca5b5d8d3c21
digi2303 <132196028+digi2303@users.noreply.github.com>
2026-09-03 15:17:22 +0300

    port armor items to vanilla equipment properties

--- src/main/java/blueportal/finsandstails/common/items/FwingedBootsItem.java
+++ src/main/java/blueportal/finsandstails/common/items/FwingedBootsItem.java
@@ -1,86 +1,67 @@
 package blueportal.finsandstails.common.items;
 
-import com.google.common.collect.ImmutableMultimap;
-import com.google.common.collect.Multimap;
-import net.minecraft.client.model.HumanoidModel;
+import blueportal.finsandstails.FinsAndTails;
+import blueportal.finsandstails.registry.FTEnchantments;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.MoverType;
-import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.equipment.ArmorType;
-import net.minecraft.world.item.equipment.ArmorMaterial;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.item.equipment.ArmorMaterial;
+import net.minecraft.world.item.equipment.ArmorType;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.common.ForgeMod;
-import net.minecraftforge.common.util.Lazy;
-import org.jetbrains.annotations.NotNull;
-import blueportal.finsandstails.FinsAndTails;
-import blueportal.finsandstails.registry.FTEnchantments;
-
-import java.util.function.Consumer;
 
 public class FwingedBootsItem extends Item {
-    public static final ArmorMaterial MATERIAL = FinsArmorMaterial.create(FinsAndTails.MOD_ID + ":fwinged", 3, new int[]{1, 2, 3, 1}, 3, SoundEvents.ARMOR_EQUIP_LEATHER, 0.0F, net.minecraft.tags.ItemTags.REPAIRS_LEATHER_ARMOR);
-    public static final Lazy<Multimap<Attribute, AttributeModifier>> MODIFIERS = Lazy.of(() ->
-            ImmutableMultimap.of(
-                    ForgeMod.SWIM_SPEED.get(), new AttributeModifier("Swim modifier", 1.25D, AttributeModifier.Operation.ADDITION),
-                    Attributes.MOVEMENT_SPEED, new AttributeModifier("Movement modifier", -0.15D, AttributeModifier.Operation.MULTIPLY_BASE)
-            ));
+    public static final ArmorMaterial MATERIAL = FinsArmorMaterial.create("fwinged", 3, new int[]{1, 2, 3, 1}, 3, SoundEvents.ARMOR_EQUIP_LEATHER, 0.0F, net.minecraft.tags.ItemTags.REPAIRS_LEATHER_ARMOR);
+    public static final ItemAttributeModifiers MODIFIERS = ItemAttributeModifiers.builder()
+            .add(Attributes.WATER_MOVEMENT_EFFICIENCY, new AttributeModifier(FinsAndTails.id("swim_modifier"), 1.0D, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.FEET)
+            .add(Attributes.MOVEMENT_SPEED, new AttributeModifier(FinsAndTails.id("movement_modifier"), -0.15D, AttributeModifier.Operation.ADD_MULTIPLIED_BASE), EquipmentSlotGroup.FEET)
+            .build();
 
     public FwingedBootsItem(Properties properties) {
-        super(properties.humanoidArmor(MATERIAL, ArmorType.BOOTS));
+        super(properties.humanoidArmor(MATERIAL, ArmorType.BOOTS).attributes(MODIFIERS));
     }
 
     @Override
-    public void onArmorTick(ItemStack stack, Level worldIn, Player player) {
-        int j = EnchantmentHelper.getItemEnchantmentLevel(FTEnchantments.FLUKED_EDGE, stack);
+    public void inventoryTick(ItemStack stack, ServerLevel worldIn, Entity entity, EquipmentSlot slot) {
+        super.inventoryTick(stack, worldIn, entity, slot);
+        if (slot != EquipmentSlot.FEET || !(entity instanceof Player player)) {
+            return;
+        }
 
-        if (EnchantmentHelper.getEnchantments(stack).containsKey(FTEnchantments.FLUKED_EDGE)) {
-            if (j <= 0 || worldIn.getBlockState(player.blockPosition().below()).is(Blocks.WATER) && worldIn.getBlockState(player.blockPosition()).isAir() && player.getDeltaMovement().y > 0.25) {
-                if (j > 0) {
-                    float f7 = player.getYRot();
-                    float f = player.getXRot();
-                    float f1 = -Mth.sin(f7 * ((float)Math.PI / 180F)) * Mth.cos(f * ((float)Math.PI / 180F));
-                    float f2 = -Mth.sin(f * ((float)Math.PI / 180F));
-                    float f3 = Mth.cos(f7 * ((float)Math.PI / 180F)) * Mth.cos(f * ((float)Math.PI / 180F));
-                    float f4 = Mth.sqrt(f1 * f1 + f2 * f2 + f3 * f3);
-                    float f5 = 3.0F * ((1.0F + (float)j) / 4.0F);
-                    f1 = f1 * (f5 / f4);
-                    f2 = f2 * (f5 / f4);
-                    f3 = f3 * (f5 / f4);
-                    if (j > 1) player.push((double) f1 / 3.5, (double) f2 / 2, (double) f3 / 3.5);
-                    else player.push((double) f1 / 1.5, (double) f2 / 2, (double) f3 / 1.5);
-                    player.startAutoSpinAttack(1);
+        int j = EnchantmentHelper.getItemEnchantmentLevel(FTEnchantments.get(worldIn.registryAccess(), FTEnchantments.FLUKED_EDGE), stack);
 
-                    if (player.onGround()) {
-                        player.move(MoverType.SELF, new Vec3(0.0D, 1.1999999F, 0.0D));
-                    }
+        if (j > 0) {
+            if (worldIn.getBlockState(player.blockPosition().below()).is(Blocks.WATER) && worldIn.getBlockState(player.blockPosition()).isAir() && player.getDeltaMovement().y > 0.25) {
+                float f7 = player.getYRot();
+                float f = player.getXRot();
+                float f1 = -Mth.sin(f7 * ((float)Math.PI / 180F)) * Mth.cos(f * ((float)Math.PI / 180F));
+                float f2 = -Mth.sin(f * ((float)Math.PI / 180F));
+                float f3 = Mth.cos(f7 * ((float)Math.PI / 180F)) * Mth.cos(f * ((float)Math.PI / 180F));
+                float f4 = Mth.sqrt(f1 * f1 + f2 * f2 + f3 * f3);
+                float f5 = 3.0F * ((1.0F + (float)j) / 4.0F);
+                f1 = f1 * (f5 / f4);
+                f2 = f2 * (f5 / f4);
+                f3 = f3 * (f5 / f4);
+                if (j > 1) player.push((double) f1 / 3.5, (double) f2 / 2, (double) f3 / 3.5);
+                else player.push((double) f1 / 1.5, (double) f2 / 2, (double) f3 / 1.5);
+                player.startAutoSpinAttack(1, 0.0F, stack);
+
+                if (player.onGround()) {
+                    player.move(MoverType.SELF, new Vec3(0.0D, 1.1999999F, 0.0D));
                 }
             }
         }
-    }
-
-    @Override
-    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
-        return slot == EquipmentSlot.FEET ? MODIFIERS.get() : super.getAttributeModifiers(slot, stack);
-    }
-
-    @Override
-    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
-        return enchantment != Enchantments.DEPTH_STRIDER && super.canApplyAtEnchantingTable(stack, enchantment);
     }
 
 }
--- src/main/java/blueportal/finsandstails/common/items/SpindlyCharmItem.java
+++ src/main/java/blueportal/finsandstails/common/items/SpindlyCharmItem.java
@@ -2,37 +2,34 @@ package blueportal.finsandstails.common.items;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import blueportal.finsandstails.FinsAndTails;
+import net.minecraft.world.item.component.TooltipDisplay;
+import net.minecraft.world.item.equipment.ArmorType;
+import blueportal.finsandstails.registry.FTTags;
 
-import java.util.List;
 import java.util.function.Consumer;
 
-public class SpindlyCharmItem extends ArmorItem {
+public class SpindlyCharmItem extends Item {
     private final String materialName;
-    private final MobEffect effect;
+    private final Holder<MobEffect> effect;
 
-    public SpindlyCharmItem(String materialName, MobEffect effect) {
-        super(
-                new FinsArmorMaterial(FinsAndTails.MOD_ID + ":spindly_" + materialName + "_charm", 1, new int[]{1, 2, 3, 1}, 8, SoundEvents.ARMOR_EQUIP_CHAIN, 0.0F, () -> Ingredient.EMPTY),
-                Type.CHESTPLATE,
-                new Properties().durability(25).rarity(Rarity.UNCOMMON)
-        );
+    public SpindlyCharmItem(String materialName, Holder<MobEffect> effect, Properties properties) {
+        super(properties
+                .humanoidArmor(FinsArmorMaterial.create("spindly_" + materialName + "_charm", 1, new int[]{1, 2, 3, 1}, 8, SoundEvents.ARMOR_EQUIP_CHAIN, 0.0F, FTTags.REPAIRS_SPINDLY_CHARM), ArmorType.CHESTPLATE)
+                .durability(25)
+                .rarity(Rarity.UNCOMMON));
         this.materialName = materialName;
         this.effect = effect;
     }
@@ -49,11 +46,16 @@ public class SpindlyCharmItem extends ArmorItem {
     }
 
     @Override
-    public void onArmorTick(ItemStack stack, Level world, Player player) {
-        if (player.isAlive() && player.getHealth() <= 8.0F && !player.getCooldowns().isOnCooldown(this)) {
+    public void inventoryTick(ItemStack stack, ServerLevel level, Entity entity, EquipmentSlot slot) {
+        super.inventoryTick(stack, level, entity, slot);
+        if (slot != EquipmentSlot.CHEST || !(entity instanceof Player player)) {
+            return;
+        }
+
+        if (player.isAlive() && player.getHealth() <= 8.0F && !player.getCooldowns().isOnCooldown(stack)) {
             player.addEffect(new MobEffectInstance(effect, 100, 0, false, false, true));
-            stack.hurtAndBreak(1, player, e -> e.broadcastBreakEvent(EquipmentSlot.CHEST));
-            player.getCooldowns().addCooldown(this, 200);
+            stack.hurtAndBreak(1, player, EquipmentSlot.CHEST);
+            player.getCooldowns().addCooldown(stack, 200);
         }
     }
 
